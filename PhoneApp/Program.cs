bd672b24161f53abed525fbda7ba9fe170252cbe
ArthurProgger <sanyamalkov228@gmail.com>
2025-06-25 17:39:49 +0500

    fix: editted a  collection initializing.

--- PhoneApp/Program.cs
+++ PhoneApp/Program.cs
@@ -1,38 +1,36 @@
-﻿using System;
+﻿using PhoneApp.Domain;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Plugin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PhoneApp.Plugin;
-using PhoneApp.Domain;
-using PhoneApp.Domain.DTO;
 
 namespace PhoneApp
 {
-  class Program
-  {
-    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-    static void Main(string[] args)
+    class Program
     {
-      Logger.Configure();
-      logger.Info("Application started");
-      try {
-        Loader.LoadPlugins();
-      } catch(Exception ex)
-      {
-        logger.Error(ex.Message);
-        logger.Trace(ex.StackTrace);
-      }
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Logger.Configure();
+            logger.Info("Application started");
+            try
+            {
+                Loader.LoadPlugins();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                logger.Trace(ex.StackTrace);
+            }
 
-      List<EmployeesDTO> dto = new List<EmployeesDTO>();
+            List<EmployeesDTO> dto = new List<EmployeesDTO>();
 
-      foreach(var plugin in Loader.Plugins)
-      {
-         dto = plugin.Run(dto).Cast<EmployeesDTO>().ToList();
-      }
+            foreach (var plugin in Loader.Plugins)
+                dto.AddRange(plugin.Run(dto).Cast<EmployeesDTO>());
 
-      Console.WriteLine("Press any key to close application...");
-      Console.ReadKey();
+            Console.WriteLine("Press any key to close application...");
+            Console.ReadKey();
+        }
     }
-  }
 }
